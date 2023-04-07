e19f3b57f50aa60ea18fe31d2ed169feee240681
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-04-07 14:58:47 -0500

    updated ports

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const userRoutes = require('./routes/user') 
 
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 
 // middlewares
 app.use(express.json());
